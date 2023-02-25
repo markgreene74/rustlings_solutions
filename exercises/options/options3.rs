31993fc70b6d27f99fcc5ddfc0fb50fbf81b4fbd
markgreene74 <markgreene74@users.noreply.github.com>
2023-02-25 15:07:56 +0000

    completed exercises/options

---
+++ exercises/options/options3.rs
@@ -0,0 +1,25 @@
+// options3.rs
+// Execute `rustlings hint options3` or use the `hint` watch subcommand for a hint.
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn main() {
+    let y: Option<Point> = Some(Point { x: 100, y: 200 });
+
+    match y {
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        _ => println!("no match"),
+    }
+
+// can also be done with 'if let', see below
+//    if let Some(ref p) = y {
+//        println!("Co-ordinates are {},{} ", p.x, p.y)
+//    } else {
+//        println!("no match")
+//    }
+
+    y; // Fix without deleting this line.
+}
