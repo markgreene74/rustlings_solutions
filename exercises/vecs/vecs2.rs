b0d81764f593b0e6a3cc7387fb09453b418d3ff7
markgreene74 <markgreene74@users.noreply.github.com>
2023-03-21 21:19:34 +0000

    updated exercises/vecs solution

--- exercises/vecs/vecs2.rs
+++ exercises/vecs/vecs2.rs
@@ -7,10 +7,10 @@
 // Execute `rustlings hint vecs2` or use the `hint` watch subcommand for a hint.
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
-    for i in v.iter_mut() {
+    for element in v.iter_mut() {
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
-        std::mem::replace(i, *i * 2);
+        std::mem::replace(element, *element * 2);
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
@@ -18,10 +18,10 @@ fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
 }
 
 fn vec_map(v: &Vec<i32>) -> Vec<i32> {
-    v.iter().map(|num| {
+    v.iter().map(|element| {
         // TODO: Do the same thing as above - but instead of mutating the
         // Vec, you can just return the new number!
-        num * 2
+        element * 2
     }).collect()
 }
 
